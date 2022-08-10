0acdd3fe9d76e90b877d0cb440a6e8557c36b029
hshine1226 <hshine1226@gmail.com>
2022-08-10 11:40:49 +0900

    Inject ClientProxy instance

--- apps/nestjs-microservices/src/app.service.ts
+++ apps/nestjs-microservices/src/app.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 import { CreateUserRequest } from './dto/create-user-request.dto';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('COMMUNICATION') private readonly communicationClient: ClientProxy,
+  ) {}
+
   private readonly users: CreateUserRequest[] = [];
 
   getHello(): string {

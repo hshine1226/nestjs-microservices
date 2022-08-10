6716597459b765ed43de70ab5f9ce528f519719c
hshine1226 <hshine1226@gmail.com>
2022-08-10 13:59:37 +0900

    Add Hybrid Application on analytics app

--- apps/analytics/src/main.ts
+++ apps/analytics/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AnalyticsModule } from './analytics.module';
 
+// Hybrid Application
+// HTTP 요청을 수신함과 동시에 Microservice를 사용하는 Application
+// https://docs.nestjs.com/faq/hybrid-application
 async function bootstrap() {
   const app = await NestFactory.create(AnalyticsModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: { port: 3001 },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3001);
 }
 bootstrap();

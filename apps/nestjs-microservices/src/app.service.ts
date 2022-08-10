36d981429ac58f01cc575f6a6a99c0a8e877fc4d
hshine1226 <hshine1226@gmail.com>
2022-08-10 11:38:28 +0900

    Add createUser service, controller

--- apps/nestjs-microservices/src/app.service.ts
+++ apps/nestjs-microservices/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateUserRequest } from './dto/create-user-request.dto';
+
 @Injectable()
 export class AppService {
+  private readonly users: CreateUserRequest[] = [];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createUser(createUserRequest: CreateUserRequest) {
+    this.users.push(createUserRequest);
   }
 }
